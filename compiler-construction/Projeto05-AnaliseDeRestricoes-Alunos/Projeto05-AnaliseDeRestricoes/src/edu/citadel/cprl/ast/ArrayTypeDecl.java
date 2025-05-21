93bc224a9c0d1d785faf837c7b073211a8ad7a3c
André <andre.lyra@protonmail.com>
2025-05-21 11:14:36 -0300

    ADDED: Projeto05-AnaliseDeRestricoes

---
+++ compiler-construction/Projeto05-AnaliseDeRestricoes-Alunos/Projeto05-AnaliseDeRestricoes/src/edu/citadel/cprl/ast/ArrayTypeDecl.java
@@ -0,0 +1,52 @@
+package edu.citadel.cprl.ast;
+
+import edu.citadel.compiler.ConstraintException;
+import edu.citadel.compiler.ErrorHandler;
+import edu.citadel.cprl.ArrayType;
+import edu.citadel.cprl.Token;
+import edu.citadel.cprl.Type;
+
+/**
+ * The abstract syntax tree node for an array type declaration.
+ */
+public class ArrayTypeDecl extends InitialDecl {
+
+    private ConstValue numElements;
+
+    /**
+     * Construct an ArrayTypeDecl with its identifier, element type, and number
+     * of elements. Note that the index type is always Integer in CPRL.
+     */
+    public ArrayTypeDecl( Token typeId, Type elemType, ConstValue numElements ) {
+        super( typeId, new ArrayType( typeId.getText(), numElements.getLiteralIntValue(), elemType ) );
+        this.numElements = numElements;
+    }
+
+    public ConstValue getNumElements() {
+        return numElements;
+    }
+    
+    @Override
+    public void checkConstraints() {
+        
+        try {
+            
+            numElements.checkConstraints();
+
+            if ( numElements.getType() != Type.Integer) {
+                String errorMsg = "Index expression must have type Integer.";
+                throw error( numElements.getPosition(), errorMsg );
+            }
+            
+            if(numElements.getLiteralIntValue() <= 0) {
+                String errorMsg = "Invalid constant.";
+                throw error( numElements.getPosition(), errorMsg );
+            }
+            
+        } catch ( ConstraintException e ) {
+            ErrorHandler.getInstance().reportError( e );
+        }
+        
+    }
+    
+}
